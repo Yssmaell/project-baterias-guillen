2258af155c8a762d1ed5aa2419d772a4680a4a89
Yssmaell <you@example.com>
2020-10-21 22:36:01 -0300

    cambios de responsive para telefono

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {  
 var currentScrollPos = window.pageYOffset;
+var width = window.innerWidth;
 var d = document.getElementById('carouselControlsIndex');
-// console.log(d.offsetTop + d.clientHeight);
-// console.log(currentScrollPos);
 var prevScrollpos = d.offsetTop + (d.clientHeight/2);
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos && width > 768) {
     document.getElementById("side-bar").style.right = "-150px";
   } else {
     document.getElementById("side-bar").style.right = "0px";
