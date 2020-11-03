e7ca8ed63ecec41a175cf8edcfd13025f29c7f36
Yssmaell <you@example.com>
2020-11-03 16:48:02 -0300

    cambios para hacer la navegacion de tlf mas amigable

--- app.js
+++ app.js
@@ -12,3 +12,19 @@ var prevScrollpos = d.offsetTop + (d.clientHeight/2);
 }
 
 
+var modal = document.getElementById("exampleModal");
+
+modal.onhide = function(){
+  document.getElementById("side-bar").style.right = "0px";
+};
+
+modal.onshown  = function(){
+  document.getElementById("side-bar").style.right = "-150px";
+};
+
+
+// window.addEventListener("shown.bs.modal", function(){
+//   modalaaa.modal('shown');
+//   alert("I want this to appear after the modal has opened!");
+// });
+
